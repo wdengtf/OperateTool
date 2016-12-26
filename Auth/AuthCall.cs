fe37b9c7d86cfcedac9d8c36c17111e2bf90dde0
wdengtf <wdeng@foxmail.com>
2016-12-26 10:25:29 +0800

    new

---
+++ Auth/AuthCall.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Events;
+using Framework.Log;
+
+namespace Auth
+{
+    public class AuthCall<T,M> where T : class where M :class
+    {
+        private readonly IAuth<T,M> iAuth = null;
+        //private readonly IEvent events = new MsmqEvents();
+        private readonly IEvent events = new Log4NetEvents();
+
+        public AuthCall()
+        {
+            iAuth = Factory.Auth<T,M>();
+        }
+
+        /// <summary>
+        /// 设置参数
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="channelUser"></param>
+        public void Set(T req, string channelUser)
+        {
+            iAuth.Set(req, channelUser);
+        }
+
+        /// <summary>
+        /// 执行 返回结果
+        /// </summary>
+        /// <returns></returns>
+        public M Auth()
+        {
+            DeleteEvent();
+            AddEvent();
+            return iAuth.Auth();
+        }
+
+        /// <summary>
+        /// 获取错误消息
+        /// </summary>
+        /// <returns></returns>
+        public string GetMessage()
+        {
+            string message = "";
+            try
+            {
+                message = iAuth.GetMessage();
+            }
+            catch (Exception ex)
+            {
+                LogService.LogDebug(ex);
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// 返回执行结果状态
+        /// </summary>
+        /// <returns></returns>
+        public bool GetResultState()
+        {
+            bool result = false;
+            try
+            {
+                result = iAuth.GetResultState();
+            }
+            catch (Exception ex)
+            {
+                LogService.LogDebug(ex);
+            }
+            return result;
+        }
+
+        #region 事件操作
+        /// <summary>
+        /// 添加事件
+        /// </summary>
+        private void AddEvent()
+        {
+            iAuth.OnBegin += new EventHandler(events.OnBegin);
+            iAuth.OnTipMsg += new EventHandler(events.OnTipMsg);
+            iAuth.OnSuccess += new EventHandler(events.OnSuccess);
+            iAuth.OnFail += new EventHandler(events.OnFail);
+            iAuth.OnException += new EventHandler(events.OnException);
+            iAuth.OnCompelete += new EventHandler(events.OnCompelete);
+        }
+
+        /// <summary>
+        /// 取消事件
+        /// </summary>
+        private void DeleteEvent()
+        {
+            iAuth.OnBegin -= new EventHandler(events.OnBegin);
+            iAuth.OnTipMsg -= new EventHandler(events.OnTipMsg);
+            iAuth.OnException -= new EventHandler(events.OnException);
+            iAuth.OnSuccess -= new EventHandler(events.OnSuccess);
+            iAuth.OnFail -= new EventHandler(events.OnFail);
+            iAuth.OnCompelete -= new EventHandler(events.OnCompelete);
+        }
+        #endregion
+
+    }
+}
